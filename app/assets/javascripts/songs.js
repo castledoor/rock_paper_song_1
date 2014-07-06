225aa6fb22c61d5cd90f4ea675f49574b094b471
castledoor <b.jazzy.w@gmail.com>
2014-07-06 12:20:42 -0700

    adds javascript for streaming

---
+++ app/assets/javascripts/songs.js
@@ -0,0 +1,18 @@
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).ready(function() {
+    var audioSection = $('section#audio');
+    $('a.html5').click(function() {
+ 
+        var audio = $('<audio>', {
+             controls : 'controls'
+        });
+ 
+        var url = $(this).attr('href');
+        $('<source>').attr('src', url).appendTo(audio);
+        audioSection.html(audio);
+        return false;
+    });
+});
